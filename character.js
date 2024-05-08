ad18ca6cd579f3f7f3b7e1e22fe93a273aeaa9b3
GroHaul <gavingeraldo.2005@gmail.com>
2024-05-08 11:24:17 +0700

    character page baru

--- character.js
+++ character.js
@@ -1,16 +1,20 @@
-$(document).ready(function(){
-    $('.carousel').carousel();
-});
-
 function changeBg(bg, title) {
     const banner = document.querySelector('.banner');
     const contents = document.querySelectorAll('.content');
+    const carouselItem = document.querySelectorAll('.carousel-item')
 
     banner.style.background = `url('./Asset/Character/${bg}')`;
     banner.style.backgroundSize = 'cover';
     banner.style.backgroundPosition = 'center';
 
     contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+
+    carouselItem.forEach(content => {
         content.classList.remove('active');
         if (content.classList.contains(title)) {
             content.classList.add('active');
