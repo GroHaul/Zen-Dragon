c5c5b7f1bcf90e0a863421eb0ee92832ff37399c
Bodhi <flavianus.thamzir@binus.ac.id>
2024-05-26 00:08:25 +0700

    Ubah Class Page

---
+++ class.js
@@ -0,0 +1,23 @@
+function changeBg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+    const carouselItem = document.querySelectorAll('.carousel-item')
+
+    banner.style.background = `url('./Asset/Elements/${bg}')`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+
+    carouselItem.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+}
