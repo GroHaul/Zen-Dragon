4f4cb34c2dcfcbc72623e6a6a8fed4e19e44cdb6
Bodhi <flavianus.thamzir@binus.ac.id>
2024-05-29 14:40:49 +0700

    FINAL_FINAL

--- character.js
+++ character.js
@@ -21,19 +21,3 @@ function changeBg(bg, title) {
         }
     });
 }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     console.log("DOM fully loaded and parsed");
-//     const toggleButton = document.querySelector('.toggle-button')
-//     const toggleButtonIcon = document.querySelector('.toggle-button i')
-//     const dropDownMenu = document.querySelector('.dropdown-menu');
-  
-//     toggleButton.onclick = function () {
-//         dropDownMenu.classList.toggle('open');
-//         const isOpen = dropDownMenu.classList.contains('open');
-  
-//         toggleButtonIcon.classList = isOpen
-//             ? 'fas fa-xmark'
-//             : 'fas fa-bars'
-//     }
-//   });
